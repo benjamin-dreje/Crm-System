6a621ec989722ff21eef2794692c8593fb1b4945
benjaminDreje <benjamindreje@gmail.com>
2026-06-22 16:55:22 +0300

    auth admin

--- backend/middleware/authAdmin.js
+++ backend/middleware/authAdmin.js
@@ -1,20 +1,16 @@
-// Middleware שחוסם את הראוט למי שאינו אדמין
 export function adminMiddleware(req, res, next) {
-  // 1. בדיקה שהמשתמש בכלל עבר את ה-authMiddleware הרגיל וקיים על ה-req
   if (!req.user) {
     return res
       .status(401)
       .json({ message: "Unauthorized: No user information found" });
   }
 
-  // 2. בדיקה האם התפקיד שלו הוא admin
   if (req.user.role !== "admin") {
     return res
       .status(403)
       .json({ message: "Forbidden: Admin access required" });
   }
 
-  // 3. אם הוא אדמין, ממשיכים לקונטרולר!
   next();
 }
 
