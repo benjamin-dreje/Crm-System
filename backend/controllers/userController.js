00b6b0b0b2496cfe98e9187e6b3d79826e367f3d
benjaminDreje <benjamindreje@gmail.com>
2026-07-04 11:15:38 +0300

    path refresh token fixed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -111,7 +111,7 @@ export const loginUser = async (req, res) => {
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 ימים
       secure: isProduction,
       sameSite: isProduction ? "strict" : "lax",
-      path: "/api/users/refresh", // חשוב להגביל את ה-Refresh Token רק לנתיב החידוש שלו
+      path: "/", // חשוב להגביל את ה-Refresh Token רק לנתיב החידוש שלו
     });
 
     res.status(200).json({
